2b902a5feeb8481078753ce7e67bd4ad493b15e0
dxfuryman <94210130+DXFURYMAN@users.noreply.github.com>
2024-07-02 14:12:08 +0530

    dev commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import logoImg from '../assets/quiz-logo.png'
 
 export const Header = () => {
     return <header>
-        <img src={logoImg}/>
+        <img src={logoImg} alt="Quiz logo"/>
         <h1 > ReactQuiz</h1>
     </header>
 }
