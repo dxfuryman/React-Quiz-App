4d25d7308178a35d343a3a22e6c75a458820bc9a
dxfuryman <94210130+DXFURYMAN@users.noreply.github.com>
2024-07-02 22:51:18 +0530

    dev commit

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -4,5 +4,5 @@ import {useState} from "react";
 export default function Quiz (){
 const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
 const [userAnswer, setUserAnswer] = useState([]);
-    return
+    return <p>Currently active Question</p>;
 }
