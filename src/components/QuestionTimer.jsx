2308286b6c755863b2e86f91a5770ea5d8374973
dxfuryman <94210130+DXFURYMAN@users.noreply.github.com>
2024-07-04 17:54:03 +0530

    dev commit

--- src/components/QuestionTimer.jsx
+++ src/components/QuestionTimer.jsx
@@ -1,18 +1,25 @@
-\]; import {useEffect, useState} from "react";
+import {useEffect, useState} from "react";
 
 
 export default function QuestionTimer({timeout, ontimeout}){
     const [remainingTime, setRemainingTime] = useState(timeout);
     useEffect(()  => {
-        console.log("SETTING TIMEOUT")
-        setTimeout(ontimeout,timeout);
+        console.log("SETTING TIMEOUT");
+        const timer = setTimeout(ontimeout,timeout);
+        return () => {
+            clearTimeout(timer);
+        }
     }, [timeout , ontimeout]);
 
     useEffect(() => {
-        setInterval(() =>{
+        const interval = setInterval(() =>{
             console.log("SETTING INTERVAL")
             setRemainingTime(prevRemainingTime => prevRemainingTime - 100);
         },100);
+
+        return () => {
+            clearInterval(interval);
+        }
     }, []);
 
     return <progress id="question-time" max={timeout} value={remainingTime}/>
