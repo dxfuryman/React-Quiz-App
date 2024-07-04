9f33d9689659d5be4661139625c2802f09a13e49
dxfuryman <94210130+DXFURYMAN@users.noreply.github.com>
2024-07-04 10:06:32 +0530

    dev commit

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -6,18 +6,27 @@ export default function Quiz (){
 const [userAnswer, setUserAnswer] = useState([]);
 
 const activeQuestionIndex = userAnswer.length;
+const shuffledAnswer = [...QUESTIONS[activeQuestionIndex].answers];
+shuffledAnswer.sort(() => Math.random() - 0.5);
+const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
 function handleSelectAnswer(selectedAnswer){
 setUserAnswer((prevUserAnswer) => {
     return[...prevUserAnswer,selectedAnswer];
 })
 }
+if (quizIsComplete){
+    return <div id="summary">
+        <img src={completed}/>
+        <h2>Quiz Completed!</h2>
+    </div>
+}
     return (
         <div id="quiz">
     <div id = "question">
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-            {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+            {shuffledAnswer.map((answer) => (
                 <li key={answer} className="answer">
                     <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
                 </li>
